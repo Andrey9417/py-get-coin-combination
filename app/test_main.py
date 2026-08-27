2809b81a177295c85b8f6395044947cde8069c48
Andrey <andreypovlat@gmail.com>
2026-08-27 18:18:12 +0300

    Task: Get coin combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,19 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, result",
+    [
+        pytest.param(1, [1, 0, 0, 0], id="1 coin type"),
+        pytest.param(6, [1, 1, 0, 0], id="2 coin type"),
+        pytest.param(17, [2, 1, 1, 0], id="3 coin type"),
+        pytest.param(41, [1, 1, 1, 1], id="4 coin type"),
+        pytest.param(100, [0, 0, 0, 4], id="highest coin type is counted first"),
+        pytest.param(0, [0, 0, 0, 0], id="zero cents"),
+
+    ]
+)
+def test_get_coin_combination(cents: int, result: list) -> None:
+    assert get_coin_combination(cents) == result
